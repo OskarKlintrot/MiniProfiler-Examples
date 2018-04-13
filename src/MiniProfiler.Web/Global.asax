09c571f239ea84cb4953b1989eb86bf7e456193e
Oskar Klintrot <oskar.klintrot@gmail.com>
2018-04-13 08:46:08 +0200

    feat: MiniProfiler for MVC

---
+++ src/MiniProfiler.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniProfiler.Web.MvcApplication" Language="C#" %>
---
+++ src/MiniProfiler.Web/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MiniProfiler.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)
+            {
+                StackExchange.Profiling.MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            if (Request.IsLocal)
+            {
+                StackExchange.Profiling.MiniProfiler.Stop();
+            }
+        }
+    }
+}
